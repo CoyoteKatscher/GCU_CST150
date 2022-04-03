2f69008f0eb5cd5be69e609bc174b6bccef29586
Brian Potter <90872695+CoyoteKatscher@users.noreply.github.com>
2022-04-03 14:12:55 -0700

    Add files via upload

---
+++ Milestone 2/Entry.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Milestone1
+{
+    partial class EntryMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entryTitleScreenLabel = new System.Windows.Forms.Label();
+            this.entryLabel = new System.Windows.Forms.Label();
+            this.serialNumberLabel = new System.Windows.Forms.Label();
+            this.serialNumberTextBox = new System.Windows.Forms.TextBox();
+            this.trunkedIDLabel = new System.Windows.Forms.Label();
+            this.trunkedRadioIDTextBox = new System.Windows.Forms.TextBox();
+            this.newButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.conventionalIDLabel = new System.Windows.Forms.Label();
+            this.conventionalRadioIDTextBox = new System.Windows.Forms.TextBox();
+            this.mdcLabel = new System.Windows.Forms.Label();
+            this.mdcCodeTextBox = new System.Windows.Forms.TextBox();
+            this.modelLabel = new System.Windows.Forms.Label();
+            this.modelNumTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.radioDescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.programmingLabel = new System.Windows.Forms.Label();
+            this.trunkSystemsLabel = new System.Windows.Forms.Label();
+            this.rwcCheckBox = new System.Windows.Forms.CheckBox();
+            this.trwcCheckBox = new System.Windows.Forms.CheckBox();
+            this.mcwsCheckBox = new System.Windows.Forms.CheckBox();
+            this.naricsCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // entryTitleScreenLabel
+            // 
+            this.entryTitleScreenLabel.AutoSize = true;
+            this.entryTitleScreenLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entryTitleScreenLabel.Location = new System.Drawing.Point(178, 9);
+            this.entryTitleScreenLabel.Name = "entryTitleScreenLabel";
+            this.entryTitleScreenLabel.Size = new System.Drawing.Size(184, 22);
+            this.entryTitleScreenLabel.TabIndex = 0;
+            this.entryTitleScreenLabel.Text = "CoT Radio Inventory";
+            // 
+            // entryLabel
+            // 
+            this.entryLabel.AutoSize = true;
+            this.entryLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entryLabel.Location = new System.Drawing.Point(234, 46);
+            this.entryLabel.Name = "entryLabel";
+            this.entryLabel.Size = new System.Drawing.Size(41, 19);
+            this.entryLabel.TabIndex = 1;
+            this.entryLabel.Text = "Entry";
+            // 
+            // serialNumberLabel
+            // 
+            this.serialNumberLabel.AutoSize = true;
+            this.serialNumberLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialNumberLabel.Location = new System.Drawing.Point(12, 86);
+            this.serialNumberLabel.Name = "serialNumberLabel";
+            this.serialNumberLabel.Size = new System.Drawing.Size(93, 17);
+            this.serialNumberLabel.TabIndex = 3;
+            this.serialNumberLabel.Text = "Serial Number";
+            // 
+            // serialNumberTextBox
+            // 
+            this.serialNumberTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialNumberTextBox.Location = new System.Drawing.Point(115, 78);
+            this.serialNumberTextBox.Name = "serialNumberTextBox";
+            this.serialNumberTextBox.Size = new System.Drawing.Size(160, 25);
+            this.serialNumberTextBox.TabIndex = 4;
+            // 
+            // trunkedIDLabel
+            // 
+            this.trunkedIDLabel.AutoSize = true;
+            this.trunkedIDLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trunkedIDLabel.Location = new System.Drawing.Point(32, 133);
+            this.trunkedIDLabel.Name = "trunkedIDLabel";
+            this.trunkedIDLabel.Size = new System.Drawing.Size(157, 17);
+            this.trunkedIDLabel.TabIndex = 5;
+            this.trunkedIDLabel.Text = "Trunked Systen Radio ID";
+            // 
+            // trunkedRadioIDTextBox
+            // 
+            this.trunkedRadioIDTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trunkedRadioIDTextBox.Location = new System.Drawing.Point(202, 125);
+            this.trunkedRadioIDTextBox.Name = "trunkedRadioIDTextBox";
+            this.trunkedRadioIDTextBox.Size = new System.Drawing.Size(160, 25);
+            this.trunkedRadioIDTextBox.TabIndex = 6;
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(511, 89);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(75, 23);
+            this.newButton.TabIndex = 7;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(511, 60);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // menuButton
+            // 
+            this.menuButton.Location = new System.Drawing.Point(511, 31);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(75, 23);
+            this.menuButton.TabIndex = 9;
+            this.menuButton.Text = "Main Menu";
+            this.menuButton.UseVisualStyleBackColor = true;
+            // 
+            // conventionalIDLabel
+            // 
+            this.conventionalIDLabel.AutoSize = true;
+            this.conventionalIDLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conventionalIDLabel.Location = new System.Drawing.Point(0, 160);
+            this.conventionalIDLabel.Name = "conventionalIDLabel";
+            this.conventionalIDLabel.Size = new System.Drawing.Size(189, 17);
+            this.conventionalIDLabel.TabIndex = 10;
+            this.conventionalIDLabel.Text = "Conventional System Radio ID";
+            // 
+            // conventionalRadioIDTextBox
+            // 
+            this.conventionalRadioIDTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conventionalRadioIDTextBox.Location = new System.Drawing.Point(202, 152);
+            this.conventionalRadioIDTextBox.Name = "conventionalRadioIDTextBox";
+            this.conventionalRadioIDTextBox.Size = new System.Drawing.Size(160, 25);
+            this.conventionalRadioIDTextBox.TabIndex = 11;
+            // 
+            // mdcLabel
+            // 
+            this.mdcLabel.AutoSize = true;
+            this.mdcLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mdcLabel.Location = new System.Drawing.Point(387, 160);
+            this.mdcLabel.Name = "mdcLabel";
+            this.mdcLabel.Size = new System.Drawing.Size(104, 17);
+            this.mdcLabel.TabIndex = 12;
+            this.mdcLabel.Text = "Fire MDC Code";
+            // 
+            // mdcCodeTextBox
+            // 
+            this.mdcCodeTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mdcCodeTextBox.Location = new System.Drawing.Point(491, 157);
+            this.mdcCodeTextBox.Name = "mdcCodeTextBox";
+            this.mdcCodeTextBox.Size = new System.Drawing.Size(95, 25);
+            this.mdcCodeTextBox.TabIndex = 13;
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.AutoSize = true;
+            this.modelLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modelLabel.Location = new System.Drawing.Point(132, 200);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(57, 17);
+            this.modelLabel.TabIndex = 14;
+            this.modelLabel.Text = "Model #";
+            // 
+            // modelNumTextBox
+            // 
+            this.modelNumTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modelNumTextBox.Location = new System.Drawing.Point(202, 192);
+            this.modelNumTextBox.Name = "modelNumTextBox";
+            this.modelNumTextBox.Size = new System.Drawing.Size(160, 25);
+            this.modelNumTextBox.TabIndex = 15;
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.Location = new System.Drawing.Point(114, 226);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(75, 17);
+            this.descriptionLabel.TabIndex = 16;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // radioDescriptionTextBox
+            // 
+            this.radioDescriptionTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioDescriptionTextBox.Location = new System.Drawing.Point(202, 218);
+            this.radioDescriptionTextBox.Name = "radioDescriptionTextBox";
+            this.radioDescriptionTextBox.Size = new System.Drawing.Size(160, 25);
+            this.radioDescriptionTextBox.TabIndex = 17;
+            // 
+            // programmingLabel
+            // 
+            this.programmingLabel.AutoSize = true;
+            this.programmingLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.programmingLabel.Location = new System.Drawing.Point(205, 274);
+            this.programmingLabel.Name = "programmingLabel";
+            this.programmingLabel.Size = new System.Drawing.Size(157, 19);
+            this.programmingLabel.TabIndex = 18;
+            this.programmingLabel.Text = "Radio Programming Info";
+            // 
+            // trunkSystemsLabel
+            // 
+            this.trunkSystemsLabel.AutoSize = true;
+            this.trunkSystemsLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trunkSystemsLabel.Location = new System.Drawing.Point(77, 318);
+            this.trunkSystemsLabel.Name = "trunkSystemsLabel";
+            this.trunkSystemsLabel.Size = new System.Drawing.Size(112, 17);
+            this.trunkSystemsLabel.TabIndex = 19;
+            this.trunkSystemsLabel.Text = "Trunking Systems";
+            // 
+            // rwcCheckBox
+            // 
+            this.rwcCheckBox.AutoSize = true;
+            this.rwcCheckBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rwcCheckBox.Location = new System.Drawing.Point(202, 319);
+            this.rwcCheckBox.Name = "rwcCheckBox";
+            this.rwcCheckBox.Size = new System.Drawing.Size(60, 21);
+            this.rwcCheckBox.TabIndex = 20;
+            this.rwcCheckBox.Text = "RWC";
+            this.rwcCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // trwcCheckBox
+            // 
+            this.trwcCheckBox.AutoSize = true;
+            this.trwcCheckBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trwcCheckBox.Location = new System.Drawing.Point(259, 319);
+            this.trwcCheckBox.Name = "trwcCheckBox";
+            this.trwcCheckBox.Size = new System.Drawing.Size(69, 21);
+            this.trwcCheckBox.TabIndex = 21;
+            this.trwcCheckBox.Text = "TRWC";
+            this.trwcCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // mcwsCheckBox
+            // 
+            this.mcwsCheckBox.AutoSize = true;
+            this.mcwsCheckBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mcwsCheckBox.Location = new System.Drawing.Point(334, 319);
+            this.mcwsCheckBox.Name = "mcwsCheckBox";
+            this.mcwsCheckBox.Size = new System.Drawing.Size(72, 21);
+            this.mcwsCheckBox.TabIndex = 22;
+            this.mcwsCheckBox.Text = "MCWS";
+            this.mcwsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // naricsCheckBox
+            // 
+            this.naricsCheckBox.AutoSize = true;
+            this.naricsCheckBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.naricsCheckBox.Location = new System.Drawing.Point(409, 319);
+            this.naricsCheckBox.Name = "naricsCheckBox";
+            this.naricsCheckBox.Size = new System.Drawing.Size(82, 21);
+            this.naricsCheckBox.TabIndex = 23;
+            this.naricsCheckBox.Text = "NARICS";
+            this.naricsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // EntryMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 450);
+            this.Controls.Add(this.naricsCheckBox);
+            this.Controls.Add(this.mcwsCheckBox);
+            this.Controls.Add(this.trwcCheckBox);
+            this.Controls.Add(this.rwcCheckBox);
+            this.Controls.Add(this.trunkSystemsLabel);
+            this.Controls.Add(this.programmingLabel);
+            this.Controls.Add(this.radioDescriptionTextBox);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.modelNumTextBox);
+            this.Controls.Add(this.modelLabel);
+            this.Controls.Add(this.mdcCodeTextBox);
+            this.Controls.Add(this.mdcLabel);
+            this.Controls.Add(this.conventionalRadioIDTextBox);
+            this.Controls.Add(this.conventionalIDLabel);
+            this.Controls.Add(this.menuButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.trunkedRadioIDTextBox);
+            this.Controls.Add(this.trunkedIDLabel);
+            this.Controls.Add(this.serialNumberTextBox);
+            this.Controls.Add(this.serialNumberLabel);
+            this.Controls.Add(this.entryLabel);
+            this.Controls.Add(this.entryTitleScreenLabel);
+            this.Name = "EntryMenu";
+            this.Text = "Entry Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label entryTitleScreenLabel;
+        private System.Windows.Forms.Label entryLabel;
+        private System.Windows.Forms.Label serialNumberLabel;
+        private System.Windows.Forms.TextBox serialNumberTextBox;
+        private System.Windows.Forms.Label trunkedIDLabel;
+        private System.Windows.Forms.TextBox trunkedRadioIDTextBox;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Label conventionalIDLabel;
+        private System.Windows.Forms.TextBox conventionalRadioIDTextBox;
+        private System.Windows.Forms.Label mdcLabel;
+        private System.Windows.Forms.TextBox mdcCodeTextBox;
+        private System.Windows.Forms.Label modelLabel;
+        private System.Windows.Forms.TextBox modelNumTextBox;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.TextBox radioDescriptionTextBox;
+        private System.Windows.Forms.Label programmingLabel;
+        private System.Windows.Forms.Label trunkSystemsLabel;
+        private System.Windows.Forms.CheckBox rwcCheckBox;
+        private System.Windows.Forms.CheckBox trwcCheckBox;
+        private System.Windows.Forms.CheckBox mcwsCheckBox;
+        private System.Windows.Forms.CheckBox naricsCheckBox;
+    }
+}
+
---
+++ Milestone 2/Entry.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone1
+{
+    struct Radio
+    {
+        public string modelNumber;
+        public string serialNumber;
+        public string description;
+        public int trunkedID;
+        public int conventionalID;
+        public string mdcID;
+        public bool systemRWC;
+        public bool systemTRWC;
+        public bool systemMCWS;
+        public bool systemNARICS;
+    }
+    public partial class EntryMenu : Form
+    {
+        public EntryMenu()
+        {
+            InitializeComponent();
+        }
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            Radio radioEntry;
+            int radioTrunkID;
+            int radioConventionalID;
+
+            radioEntry = new Radio();
+
+            // Add string values to string members of Radio class
+            radioEntry.modelNumber = modelNumTextBox.Text;
+            radioEntry.serialNumber = serialNumberTextBox.Text;
+            radioEntry.description = radioDescriptionTextBox.Text;
+            radioEntry.mdcID = mdcCodeTextBox.Text;
+
+            // Validate user input for int members of Radio class
+            // if valid add values to members of Radio class else display error message
+            if (int.TryParse(trunkedRadioIDTextBox.Text, out radioTrunkID))
+                radioEntry.trunkedID = radioTrunkID;
+            else
+                MessageBox.Show("Enter valid Trunked Radio ID");
+
+            if (int.TryParse(conventionalRadioIDTextBox.Text, out radioConventionalID))
+                radioEntry.conventionalID = radioConventionalID;
+            else
+                MessageBox.Show("Enter valid Conventional Radio ID");
+
+            // Check each check box to see if it is checked.
+            // If checked, make member of Radio class set to true
+            // Else check box is not checked, make member of Radio class set to false
+            if (rwcCheckBox.Checked)
+                radioEntry.systemRWC = true;
+            else
+                radioEntry.systemRWC = false;
+
+            if (trwcCheckBox.Checked)
+                radioEntry.systemTRWC = true;
+            else
+                radioEntry.systemTRWC = false;
+
+            if (mcwsCheckBox.Checked)
+                radioEntry.systemMCWS = true;
+            else
+                radioEntry.systemMCWS = false;
+
+            if (naricsCheckBox.Checked)
+                radioEntry.systemNARICS = true;
+            else
+                radioEntry.systemNARICS = false;
+
+            // Test to verify members of Radio are populated
+            MessageBox.Show("Serial Number:   " + radioEntry.serialNumber + "\n" +
+                            "Model Number:    " + radioEntry.modelNumber + "\n" +
+                            "Description:     " + radioEntry.description + "\n" +
+                            "Trunked ID:      " + radioEntry.trunkedID + "\n" +
+                            "Conventional ID: " + radioEntry.conventionalID + "\n" +
+                            "MDC ID:          " + radioEntry.mdcID + "\n" +
+                            "RWC System:      " + radioEntry.systemRWC + "\n" +
+                            "TRWC System:     " + radioEntry.systemTRWC + "\n" +
+                            "MCWS System:     " + radioEntry.systemMCWS + "\n" +
+                            "NARICS System:   " + radioEntry.systemNARICS + "\n");
+
+        }
+    }
+}
