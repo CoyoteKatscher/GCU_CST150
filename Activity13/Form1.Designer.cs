4635ed4b391f0588e1dee0714af1c90558b0d075
Brian Potter <90872695+CoyoteKatscher@users.noreply.github.com>
2022-04-16 22:59:16 -0700

    Add files via upload

---
+++ Activity13/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Activity13
+{
+    partial class Activity13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.C1R1_label = new System.Windows.Forms.Label();
+            this.C2R1_label = new System.Windows.Forms.Label();
+            this.C3R1_label = new System.Windows.Forms.Label();
+            this.C3R2_label = new System.Windows.Forms.Label();
+            this.C2R2_label = new System.Windows.Forms.Label();
+            this.C1R2_label = new System.Windows.Forms.Label();
+            this.C3R3_label = new System.Windows.Forms.Label();
+            this.C2R3_label = new System.Windows.Forms.Label();
+            this.C1R3_label = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // C1R1_label
+            // 
+            this.C1R1_label.AutoSize = true;
+            this.C1R1_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C1R1_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R1_label.Location = new System.Drawing.Point(30, 45);
+            this.C1R1_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C1R1_label.Name = "C1R1_label";
+            this.C1R1_label.Size = new System.Drawing.Size(55, 57);
+            this.C1R1_label.TabIndex = 0;
+            this.C1R1_label.Text = "T";
+            // 
+            // C2R1_label
+            // 
+            this.C2R1_label.AutoSize = true;
+            this.C2R1_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C2R1_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R1_label.Location = new System.Drawing.Point(190, 45);
+            this.C2R1_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C2R1_label.Name = "C2R1_label";
+            this.C2R1_label.Size = new System.Drawing.Size(39, 57);
+            this.C2R1_label.TabIndex = 1;
+            this.C2R1_label.Text = "I";
+            // 
+            // C3R1_label
+            // 
+            this.C3R1_label.AutoSize = true;
+            this.C3R1_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C3R1_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R1_label.Location = new System.Drawing.Point(350, 45);
+            this.C3R1_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C3R1_label.Name = "C3R1_label";
+            this.C3R1_label.Size = new System.Drawing.Size(61, 57);
+            this.C3R1_label.TabIndex = 2;
+            this.C3R1_label.Text = "C";
+            // 
+            // C3R2_label
+            // 
+            this.C3R2_label.AutoSize = true;
+            this.C3R2_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C3R2_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R2_label.Location = new System.Drawing.Point(350, 120);
+            this.C3R2_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C3R2_label.Name = "C3R2_label";
+            this.C3R2_label.Size = new System.Drawing.Size(61, 57);
+            this.C3R2_label.TabIndex = 5;
+            this.C3R2_label.Text = "C";
+            // 
+            // C2R2_label
+            // 
+            this.C2R2_label.AutoSize = true;
+            this.C2R2_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C2R2_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R2_label.Location = new System.Drawing.Point(190, 120);
+            this.C2R2_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C2R2_label.Name = "C2R2_label";
+            this.C2R2_label.Size = new System.Drawing.Size(58, 57);
+            this.C2R2_label.TabIndex = 4;
+            this.C2R2_label.Text = "A";
+            // 
+            // C1R2_label
+            // 
+            this.C1R2_label.AutoSize = true;
+            this.C1R2_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C1R2_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R2_label.Location = new System.Drawing.Point(30, 120);
+            this.C1R2_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C1R2_label.Name = "C1R2_label";
+            this.C1R2_label.Size = new System.Drawing.Size(55, 57);
+            this.C1R2_label.TabIndex = 3;
+            this.C1R2_label.Text = "T";
+            // 
+            // C3R3_label
+            // 
+            this.C3R3_label.AutoSize = true;
+            this.C3R3_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C3R3_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R3_label.Location = new System.Drawing.Point(350, 195);
+            this.C3R3_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C3R3_label.Name = "C3R3_label";
+            this.C3R3_label.Size = new System.Drawing.Size(58, 57);
+            this.C3R3_label.TabIndex = 8;
+            this.C3R3_label.Text = "E";
+            // 
+            // C2R3_label
+            // 
+            this.C2R3_label.AutoSize = true;
+            this.C2R3_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C2R3_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R3_label.Location = new System.Drawing.Point(190, 195);
+            this.C2R3_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C2R3_label.Name = "C2R3_label";
+            this.C2R3_label.Size = new System.Drawing.Size(63, 57);
+            this.C2R3_label.TabIndex = 7;
+            this.C2R3_label.Text = "O";
+            // 
+            // C1R3_label
+            // 
+            this.C1R3_label.AutoSize = true;
+            this.C1R3_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C1R3_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R3_label.Location = new System.Drawing.Point(30, 195);
+            this.C1R3_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.C1R3_label.Name = "C1R3_label";
+            this.C1R3_label.Size = new System.Drawing.Size(55, 57);
+            this.C1R3_label.TabIndex = 6;
+            this.C1R3_label.Text = "T";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(250, 275);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(17, 20);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "  ";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameButton.Location = new System.Drawing.Point(55, 325);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(4);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(125, 50);
+            this.newGameButton.TabIndex = 10;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(350, 325);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(125, 50);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Activity13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.C3R3_label);
+            this.Controls.Add(this.C2R3_label);
+            this.Controls.Add(this.C1R3_label);
+            this.Controls.Add(this.C3R2_label);
+            this.Controls.Add(this.C2R2_label);
+            this.Controls.Add(this.C1R2_label);
+            this.Controls.Add(this.C3R1_label);
+            this.Controls.Add(this.C2R1_label);
+            this.Controls.Add(this.C1R1_label);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Activity13";
+            this.Text = "Activity 13 - TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label C1R1_label;
+        private System.Windows.Forms.Label C2R1_label;
+        private System.Windows.Forms.Label C3R1_label;
+        private System.Windows.Forms.Label C3R2_label;
+        private System.Windows.Forms.Label C2R2_label;
+        private System.Windows.Forms.Label C1R2_label;
+        private System.Windows.Forms.Label C3R3_label;
+        private System.Windows.Forms.Label C2R3_label;
+        private System.Windows.Forms.Label C1R3_label;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Activity13/Form1.cs
@@ -0,0 +1,221 @@
+﻿// Brian Potter
+// CST-150
+// 04/10/2022
+// Primary souce used for project understanding is assigned textbook
+// Majority of logic used was brute force and can likely be cleaned up at a later date
+// The code is of my own work
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13
+{
+    public partial class Activity13 : Form
+    {
+        public Activity13()
+        {
+            InitializeComponent();
+        }        
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int[,] ticTacToeBoard = new int[3,3];
+            int winner = -1;
+
+            // Initialize Board to -1 for all squares
+            for (int row = 0; row < ticTacToeBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < ticTacToeBoard.GetLength(1); col++)
+                {
+                    ticTacToeBoard[row, col] = -1;
+                }
+
+            }
+
+            // Start filling in squares with random # between 0 & 1
+            // Check for a winner after each square filled
+            // Once a winner is determined the rest of the board remains unfilled
+            for (int row = 0; row < ticTacToeBoard.GetLength(0) && winner == -1; row++)
+            {
+                for (int col = 0; col < ticTacToeBoard.GetLength(1) && winner == -1; col++)
+                {
+                    ticTacToeBoard[row, col] = rand.Next(2);
+                    winner = declareWinner(ticTacToeBoard);
+                }                
+            }
+
+            // Display final board using standard Xs & Os
+            displayBoard(ticTacToeBoard);
+           
+            // Populate winner in result label
+            if (winner == 0)
+                resultLabel.Text = "O Wins!";
+            else if (winner == 1)
+                resultLabel.Text = "X Wins!";
+            else
+                resultLabel.Text = "Tie Game!";
+        }
+
+        private int declareWinner(int [,] board)
+        {
+            int winningValue = -1;
+
+            // First if checks to make sure a random number has been assigned to entire row or column
+            // Second if checks for a winning combination 
+
+            // Check for a horizontal win
+            if (board[0,1] != -1)
+            {
+                if (board[0, 0] == board[0, 1] &&
+                    board[0, 1] == board[0, 2])
+                {
+                    winningValue = board[0, 0];
+                }
+            }
+
+            if (board[1,1] != -1)
+            {
+                if (board[1, 0] == board[1, 1] &&
+                    board[1, 1] == board[1, 2])
+                {
+                    winningValue = board[1, 0];
+                }
+            }
+
+            if (board[2,1] != -1)
+            {
+                if (board[2, 0] == board[2, 1] &&
+                    board[2, 1] == board[2, 2])
+                {
+                    winningValue = board[2, 0];
+                }
+            }
+            
+            // Check for a vertical win
+            if (board[1,0] != -1)
+            {
+                if (board[0, 0] == board[1, 0] &&
+                    board[1, 0] == board[2, 0])
+                {
+                    winningValue = board[0, 0];
+                }
+            }
+
+            if (board[1,1] != -1)
+            {
+                if (board[0, 1] == board[1, 1] &&
+                    board[1, 1] == board[2, 1])
+                {
+                    winningValue = board[0, 1];
+                }
+            }
+
+            if (board[1,2] != -1)
+            {
+                if (board[0, 2] == board[1, 2] &&
+                    board[1, 2] == board[2, 2])
+                {
+                    winningValue = board[0, 2];
+                }
+            }
+
+            // Check for diagional win
+            // No need to check for non-filled in boxes since all boxes will be filled in at this point
+            if (board[0, 0] == board[1, 1] &&
+                board[1, 1] == board[2, 2])
+                {
+                    winningValue = board[0, 0];
+                }
+
+            if (board[0, 2] == board[1, 1] &&
+                    board[1, 1] == board[2, 0])
+                {
+                    winningValue = board[0, 2];
+                }
+
+            return winningValue;            
+        }
+
+        private void displayBoard(int[,] board)
+        {
+            // Convert box with a value of "0" to "O" or
+            // Convert box with a value of "1" to "X" or
+            // Convert box to blank
+            if (board[0, 0] == 0)
+                C1R1_label.Text = "O";
+            else if (board[0, 0] == 1)
+                C1R1_label.Text = "X";
+            else
+                C1R1_label.Text = "   ";
+
+            if (board[0, 1] == 0)
+                C1R2_label.Text = "O";
+            else if (board[0, 1] == 1)
+                C1R2_label.Text = "X";
+            else
+                C1R2_label.Text = "   ";
+
+            if (board[0, 2] == 0)
+                C1R3_label.Text = "O";
+            else if (board[0, 2] == 1)
+                C1R3_label.Text = "X";
+            else
+                C1R3_label.Text = "   ";
+
+            if (board[1, 0] == 0)
+                C2R1_label.Text = "O";
+            else if (board[1,0] == 1)
+                C2R1_label.Text = "X";
+            else
+                C2R1_label.Text = "   ";
+
+            if (board[1, 1] == 0)
+                C2R2_label.Text = "O";
+            else if (board[1, 1] == 1)
+                C2R2_label.Text = "X";
+            else
+                C2R2_label.Text = "   ";
+
+            if (board[1, 2] == 0)
+                C2R3_label.Text = "O";
+            else if (board[1, 2] == 1)
+                C2R3_label.Text = "X";
+            else
+                C2R3_label.Text = "   ";
+
+            if (board[2, 0] == 0)
+                C3R1_label.Text = "O";
+            else if (board[2, 0] == 1)
+                C3R1_label.Text = "X";
+            else
+                C3R1_label.Text = "   ";
+
+            if (board[2, 1] == 0)
+                C3R2_label.Text = "O";
+            else if (board[2, 1] == 1)
+                C3R2_label.Text = "X";
+            else
+                C3R2_label.Text = "   ";
+
+            if (board[2, 2] == 0)
+                C3R3_label.Text = "O";
+            else if (board[2, 2] == 1)
+                C3R3_label.Text = "X";
+            else
+                C3R3_label.Text = "   ";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
