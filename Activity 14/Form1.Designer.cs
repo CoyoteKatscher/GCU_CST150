c13bb70acc222e74a85a74ade56e90fd03d28f6c
Brian Potter <90872695+CoyoteKatscher@users.noreply.github.com>
2022-04-16 23:50:34 -0700

    Add files via upload
    CST150_OC2

---
+++ Activity 14/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Activity_14
+{
+    partial class Activity14
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.meatListBox = new System.Windows.Forms.ListBox();
+            this.cheeseChkBx = new System.Windows.Forms.CheckBox();
+            this.lettuceChkBx = new System.Windows.Forms.CheckBox();
+            this.tomatoChkBx = new System.Windows.Forms.CheckBox();
+            this.picklesChxBx = new System.Windows.Forms.CheckBox();
+            this.mayoChkBx = new System.Windows.Forms.CheckBox();
+            this.mustardChkBx = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sandwichMade = new System.Windows.Forms.Label();
+            this.makeButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.whiteSubRollRadioBtn = new System.Windows.Forms.RadioButton();
+            this.wheatSubRollRadioBtn = new System.Windows.Forms.RadioButton();
+            this.sourdoughRadioBtn = new System.Windows.Forms.RadioButton();
+            this.sandwichMadeLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // meatListBox
+            // 
+            this.meatListBox.FormattingEnabled = true;
+            this.meatListBox.ItemHeight = 19;
+            this.meatListBox.Items.AddRange(new object[] {
+            "Ham",
+            "Turkey",
+            "Roast Beef",
+            "Pastrami"});
+            this.meatListBox.Location = new System.Drawing.Point(12, 40);
+            this.meatListBox.Name = "meatListBox";
+            this.meatListBox.Size = new System.Drawing.Size(120, 80);
+            this.meatListBox.TabIndex = 0;
+            // 
+            // cheeseChkBx
+            // 
+            this.cheeseChkBx.AutoSize = true;
+            this.cheeseChkBx.Location = new System.Drawing.Point(79, 270);
+            this.cheeseChkBx.Name = "cheeseChkBx";
+            this.cheeseChkBx.Size = new System.Drawing.Size(73, 23);
+            this.cheeseChkBx.TabIndex = 4;
+            this.cheeseChkBx.Text = "Cheese";
+            this.cheeseChkBx.UseVisualStyleBackColor = true;
+            // 
+            // lettuceChkBx
+            // 
+            this.lettuceChkBx.AutoSize = true;
+            this.lettuceChkBx.Location = new System.Drawing.Point(158, 270);
+            this.lettuceChkBx.Name = "lettuceChkBx";
+            this.lettuceChkBx.Size = new System.Drawing.Size(73, 23);
+            this.lettuceChkBx.TabIndex = 5;
+            this.lettuceChkBx.Text = "Lettuce";
+            this.lettuceChkBx.UseVisualStyleBackColor = true;
+            // 
+            // tomatoChkBx
+            // 
+            this.tomatoChkBx.AutoSize = true;
+            this.tomatoChkBx.Location = new System.Drawing.Point(237, 299);
+            this.tomatoChkBx.Name = "tomatoChkBx";
+            this.tomatoChkBx.Size = new System.Drawing.Size(74, 23);
+            this.tomatoChkBx.TabIndex = 6;
+            this.tomatoChkBx.Text = "Tomato";
+            this.tomatoChkBx.UseVisualStyleBackColor = true;
+            // 
+            // picklesChxBx
+            // 
+            this.picklesChxBx.AutoSize = true;
+            this.picklesChxBx.Location = new System.Drawing.Point(158, 299);
+            this.picklesChxBx.Name = "picklesChxBx";
+            this.picklesChxBx.Size = new System.Drawing.Size(71, 23);
+            this.picklesChxBx.TabIndex = 7;
+            this.picklesChxBx.Text = "Pickles";
+            this.picklesChxBx.UseVisualStyleBackColor = true;
+            // 
+            // mayoChkBx
+            // 
+            this.mayoChkBx.AutoSize = true;
+            this.mayoChkBx.Location = new System.Drawing.Point(237, 270);
+            this.mayoChkBx.Name = "mayoChkBx";
+            this.mayoChkBx.Size = new System.Drawing.Size(64, 23);
+            this.mayoChkBx.TabIndex = 8;
+            this.mayoChkBx.Text = "Mayo";
+            this.mayoChkBx.UseVisualStyleBackColor = true;
+            // 
+            // mustardChkBx
+            // 
+            this.mustardChkBx.AutoSize = true;
+            this.mustardChkBx.Location = new System.Drawing.Point(79, 299);
+            this.mustardChkBx.Name = "mustardChkBx";
+            this.mustardChkBx.Size = new System.Drawing.Size(79, 23);
+            this.mustardChkBx.TabIndex = 9;
+            this.mustardChkBx.Text = "Mustard";
+            this.mustardChkBx.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Meat choice";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Toppings";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Your Sandwich";
+            // 
+            // sandwichMade
+            // 
+            this.sandwichMade.AutoSize = true;
+            this.sandwichMade.Location = new System.Drawing.Point(332, 78);
+            this.sandwichMade.Name = "sandwichMade";
+            this.sandwichMade.Size = new System.Drawing.Size(0, 19);
+            this.sandwichMade.TabIndex = 14;
+            // 
+            // makeButton
+            // 
+            this.makeButton.Location = new System.Drawing.Point(15, 358);
+            this.makeButton.Name = "makeButton";
+            this.makeButton.Size = new System.Drawing.Size(75, 40);
+            this.makeButton.TabIndex = 16;
+            this.makeButton.Text = "Make!";
+            this.makeButton.UseVisualStyleBackColor = true;
+            this.makeButton.Click += new System.EventHandler(this.makeButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(170, 358);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(110, 40);
+            this.clearButton.TabIndex = 17;
+            this.clearButton.Text = "Clear Selection";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(350, 358);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 40);
+            this.exitButton.TabIndex = 18;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sourdoughRadioBtn);
+            this.groupBox1.Controls.Add(this.wheatSubRollRadioBtn);
+            this.groupBox1.Controls.Add(this.whiteSubRollRadioBtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 114);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bread Choice";
+            // 
+            // whiteSubRollRadioBtn
+            // 
+            this.whiteSubRollRadioBtn.AutoSize = true;
+            this.whiteSubRollRadioBtn.Location = new System.Drawing.Point(1, 25);
+            this.whiteSubRollRadioBtn.Name = "whiteSubRollRadioBtn";
+            this.whiteSubRollRadioBtn.Size = new System.Drawing.Size(119, 23);
+            this.whiteSubRollRadioBtn.TabIndex = 0;
+            this.whiteSubRollRadioBtn.TabStop = true;
+            this.whiteSubRollRadioBtn.Text = "White Sub Roll";
+            this.whiteSubRollRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // wheatSubRollRadioBtn
+            // 
+            this.wheatSubRollRadioBtn.AutoSize = true;
+            this.wheatSubRollRadioBtn.Location = new System.Drawing.Point(1, 54);
+            this.wheatSubRollRadioBtn.Name = "wheatSubRollRadioBtn";
+            this.wheatSubRollRadioBtn.Size = new System.Drawing.Size(123, 23);
+            this.wheatSubRollRadioBtn.TabIndex = 1;
+            this.wheatSubRollRadioBtn.TabStop = true;
+            this.wheatSubRollRadioBtn.Text = "Wheat Sub Roll";
+            this.wheatSubRollRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // sourdoughRadioBtn
+            // 
+            this.sourdoughRadioBtn.AutoSize = true;
+            this.sourdoughRadioBtn.Location = new System.Drawing.Point(0, 83);
+            this.sourdoughRadioBtn.Name = "sourdoughRadioBtn";
+            this.sourdoughRadioBtn.Size = new System.Drawing.Size(134, 23);
+            this.sourdoughRadioBtn.TabIndex = 2;
+            this.sourdoughRadioBtn.TabStop = true;
+            this.sourdoughRadioBtn.Text = "Sourdough Bread";
+            this.sourdoughRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // sandwichMadeLabel
+            // 
+            this.sandwichMadeLabel.AutoSize = true;
+            this.sandwichMadeLabel.Location = new System.Drawing.Point(235, 40);
+            this.sandwichMadeLabel.Name = "sandwichMadeLabel";
+            this.sandwichMadeLabel.Size = new System.Drawing.Size(113, 19);
+            this.sandwichMadeLabel.TabIndex = 20;
+            this.sandwichMadeLabel.Text = "Nothing Selected";
+            // 
+            // Activity14
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.sandwichMadeLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.makeButton);
+            this.Controls.Add(this.sandwichMade);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mustardChkBx);
+            this.Controls.Add(this.mayoChkBx);
+            this.Controls.Add(this.picklesChxBx);
+            this.Controls.Add(this.tomatoChkBx);
+            this.Controls.Add(this.lettuceChkBx);
+            this.Controls.Add(this.cheeseChkBx);
+            this.Controls.Add(this.meatListBox);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Activity14";
+            this.Text = "Activity 14 - Sandwich Maker";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox meatListBox;
+        private System.Windows.Forms.CheckBox cheeseChkBx;
+        private System.Windows.Forms.CheckBox lettuceChkBx;
+        private System.Windows.Forms.CheckBox tomatoChkBx;
+        private System.Windows.Forms.CheckBox picklesChxBx;
+        private System.Windows.Forms.CheckBox mayoChkBx;
+        private System.Windows.Forms.CheckBox mustardChkBx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label sandwichMade;
+        private System.Windows.Forms.Button makeButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton sourdoughRadioBtn;
+        private System.Windows.Forms.RadioButton wheatSubRollRadioBtn;
+        private System.Windows.Forms.RadioButton whiteSubRollRadioBtn;
+        private System.Windows.Forms.Label sandwichMadeLabel;
+    }
+}
+
---
+++ Activity 14/Form1.cs
@@ -0,0 +1,131 @@
+﻿// Brian Potter
+// CST-150
+// 04/16/2022
+// Sources assisting with logic in clearEntries() && adding check box items to sandwichMadeLabel
+// Is it possible to append text to a string or label text programmatically? (n.d.). Retrieved April 16, 2022, from https://social.msdn.microsoft.com/Forums/en-US/5cba9f62-2bb5-498c-be21-56d9a6e54800/is-it-possible-to-append-text-to-a-string-or-label-text-programmatically?forum=aspwebforms 
+// How to clear radio buttons and checkboxes by using a menu item ? (n.d.). Retrieved April 16, 2022, from https://social.msdn.microsoft.com/Forums/sqlserver/en-US/d0976688-e366-4861-ade2-799717600303/how-to-clear-radio-buttons-and-checkboxes-by-using-a-menu-item-?forum=vbgeneral 
+// The code is of my own work
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_14
+{
+    public partial class Activity14 : Form
+    {
+        public Activity14()
+        {
+            InitializeComponent();            
+        }
+
+        private void clearEntries()
+        {
+            // Clears all of the different selections
+
+            // Clears List Box Selection
+            meatListBox.SelectedIndex = -1;
+
+            // Resets all radio button options to not be checked
+            whiteSubRollRadioBtn.Checked = false;
+            wheatSubRollRadioBtn.Checked = false;
+            sourdoughRadioBtn.Checked = false;
+
+            // Resets all check box options to not be checked
+            cheeseChkBx.Checked = false;
+            lettuceChkBx.Checked = false;
+            tomatoChkBx.Checked = false;
+            picklesChxBx.Checked = false;
+            mayoChkBx.Checked = false;
+            mustardChkBx.Checked = false;
+
+            // Clears the text from the label that lists the selected sandwich
+            sandwichMadeLabel.Text = " ";
+        }
+
+        private void makeButton_Click(object sender, EventArgs e)
+        {
+            string selectedMeat ="";
+            string selectedBread = "";
+            bool validEntry = true;
+
+            // Error Check: Verify user has selected at least one item from the List Box
+            if (meatListBox.SelectedIndex != -1)
+                selectedMeat = meatListBox.SelectedItem.ToString();
+            else
+            {
+                MessageBox.Show("Please Select a Meat");
+                clearEntries();
+                validEntry = false;
+            }
+
+            // Error Check: If user selected at least one item from the List Box then
+            // the program verifies user has selected one of the Radio Button items.
+            // If a Radio Button has been selected the value of the selected item is stored
+            // in selectedBread. 
+            // If none of the Radio Buttons have been selected then an error message is displayed
+            if (validEntry)
+            { 
+                if (whiteSubRollRadioBtn.Checked)
+                    selectedBread = "White Sub Roll";
+                if (wheatSubRollRadioBtn.Checked)
+                    selectedBread = "Wheat Sub Roll";
+                if (sourdoughRadioBtn.Checked)
+                    selectedBread = "Sourdough Bread";
+                if (!whiteSubRollRadioBtn.Checked &&
+                    !wheatSubRollRadioBtn.Checked &&
+                    !sourdoughRadioBtn.Checked)
+                {
+                    MessageBox.Show("Please Select a Bread");
+                    clearEntries();
+                    validEntry = false;
+                }
+            }
+
+            // Error Check: If user selected one item from the List Box and one of
+            // the Radio Buttons then the user can select optional items.
+            // The sandwhichMadeLabel text area is populated with the sandwhich
+            // made by the user
+            if (validEntry)
+            {
+                sandwichMadeLabel.Text = selectedMeat + " on " +
+                                         selectedBread + " with \n";
+                if (cheeseChkBx.Checked)
+                    sandwichMadeLabel.Text += "Cheese\n";
+                if (lettuceChkBx.Checked)
+                    sandwichMadeLabel.Text += "Lettuce\n";
+                if (tomatoChkBx.Checked)
+                    sandwichMadeLabel.Text += "Tomato\n";
+                if (picklesChxBx.Checked)
+                    sandwichMadeLabel.Text += "Pickles\n";
+                if (mayoChkBx.Checked)
+                    sandwichMadeLabel.Text += "Mayo\n";
+                if (mustardChkBx.Checked)
+                    sandwichMadeLabel.Text += "Mustard\n";
+                if (!cheeseChkBx.Checked && !lettuceChkBx.Checked &&
+                    !tomatoChkBx.Checked && !picklesChxBx.Checked &&
+                    !mayoChkBx.Checked && !mustardChkBx.Checked)
+                    sandwichMadeLabel.Text += "No Toppings\n";
+            }
+            else
+                clearEntries();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // All options are reset
+            clearEntries();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
