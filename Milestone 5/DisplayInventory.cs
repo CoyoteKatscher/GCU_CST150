1a4c72b4566f2a5c4cb9f379b8651fc8673eac7f
Brian Potter <90872695+CoyoteKatscher@users.noreply.github.com>
2022-04-24 16:19:38 -0700

    Add files via upload
    GCU CST150  Milestone 5

---
+++ Milestone 5/DisplayInventory.cs
@@ -0,0 +1,32 @@
+﻿// Brian Potter
+// CST-150
+// 04/24/2022
+// Radio image copyright Motorola Solutions https://www.motorolasolutions.com/en_us.html?geo=redirect
+// The code is of my own work
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone_5
+{
+    public partial class DisplayInventory : Form
+    {
+        public DisplayInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Milestone 5/Main.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Milestone_5
+{
+    partial class MilestonePrj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MilestonePrj));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.snLabel = new System.Windows.Forms.Label();
+            this.trunkedIDLabel = new System.Windows.Forms.Label();
+            this.convIDLabel = new System.Windows.Forms.Label();
+            this.mdcLabel = new System.Windows.Forms.Label();
+            this.modelNumLabel = new System.Windows.Forms.Label();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.trunkSysLabel = new System.Windows.Forms.Label();
+            this.snTextBox = new System.Windows.Forms.TextBox();
+            this.trunkedIDTextBox = new System.Windows.Forms.TextBox();
+            this.convIDTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.descTextBox = new System.Windows.Forms.TextBox();
+            this.mdcTextBox = new System.Windows.Forms.TextBox();
+            this.rwcCkBx = new System.Windows.Forms.CheckBox();
+            this.trwcCkBx = new System.Windows.Forms.CheckBox();
+            this.mcwsCkBx = new System.Windows.Forms.CheckBox();
+            this.naricsCkBx = new System.Windows.Forms.CheckBox();
+            this.newButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.radioPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.radioPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(247, 9);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(169, 27);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Radio Inventory";
+            // 
+            // snLabel
+            // 
+            this.snLabel.AutoSize = true;
+            this.snLabel.Location = new System.Drawing.Point(70, 60);
+            this.snLabel.Name = "snLabel";
+            this.snLabel.Size = new System.Drawing.Size(97, 19);
+            this.snLabel.TabIndex = 1;
+            this.snLabel.Text = "Serial Number";
+            // 
+            // trunkedIDLabel
+            // 
+            this.trunkedIDLabel.AutoSize = true;
+            this.trunkedIDLabel.Location = new System.Drawing.Point(40, 115);
+            this.trunkedIDLabel.Name = "trunkedIDLabel";
+            this.trunkedIDLabel.Size = new System.Drawing.Size(127, 19);
+            this.trunkedIDLabel.TabIndex = 2;
+            this.trunkedIDLabel.Text = "Trunked System ID";
+            // 
+            // convIDLabel
+            // 
+            this.convIDLabel.AutoSize = true;
+            this.convIDLabel.Location = new System.Drawing.Point(11, 150);
+            this.convIDLabel.Name = "convIDLabel";
+            this.convIDLabel.Size = new System.Drawing.Size(156, 19);
+            this.convIDLabel.TabIndex = 3;
+            this.convIDLabel.Text = "Conventional System ID";
+            // 
+            // mdcLabel
+            // 
+            this.mdcLabel.AutoSize = true;
+            this.mdcLabel.Location = new System.Drawing.Point(360, 150);
+            this.mdcLabel.Name = "mdcLabel";
+            this.mdcLabel.Size = new System.Drawing.Size(111, 19);
+            this.mdcLabel.TabIndex = 4;
+            this.mdcLabel.Text = "Fire MDC Code";
+            // 
+            // modelNumLabel
+            // 
+            this.modelNumLabel.AutoSize = true;
+            this.modelNumLabel.Location = new System.Drawing.Point(64, 200);
+            this.modelNumLabel.Name = "modelNumLabel";
+            this.modelNumLabel.Size = new System.Drawing.Size(103, 19);
+            this.modelNumLabel.TabIndex = 5;
+            this.modelNumLabel.Text = "Model Number";
+            // 
+            // descLabel
+            // 
+            this.descLabel.AutoSize = true;
+            this.descLabel.Location = new System.Drawing.Point(89, 235);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(78, 19);
+            this.descLabel.TabIndex = 6;
+            this.descLabel.Text = "Description";
+            // 
+            // trunkSysLabel
+            // 
+            this.trunkSysLabel.AutoSize = true;
+            this.trunkSysLabel.Location = new System.Drawing.Point(52, 300);
+            this.trunkSysLabel.Name = "trunkSysLabel";
+            this.trunkSysLabel.Size = new System.Drawing.Size(115, 19);
+            this.trunkSysLabel.TabIndex = 7;
+            this.trunkSysLabel.Text = "Trunking Systems";
+            // 
+            // snTextBox
+            // 
+            this.snTextBox.Location = new System.Drawing.Point(173, 57);
+            this.snTextBox.Name = "snTextBox";
+            this.snTextBox.Size = new System.Drawing.Size(200, 26);
+            this.snTextBox.TabIndex = 8;
+            // 
+            // trunkedIDTextBox
+            // 
+            this.trunkedIDTextBox.Location = new System.Drawing.Point(173, 112);
+            this.trunkedIDTextBox.Name = "trunkedIDTextBox";
+            this.trunkedIDTextBox.Size = new System.Drawing.Size(150, 26);
+            this.trunkedIDTextBox.TabIndex = 9;
+            // 
+            // convIDTextBox
+            // 
+            this.convIDTextBox.Location = new System.Drawing.Point(173, 143);
+            this.convIDTextBox.Name = "convIDTextBox";
+            this.convIDTextBox.Size = new System.Drawing.Size(150, 26);
+            this.convIDTextBox.TabIndex = 10;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Location = new System.Drawing.Point(173, 197);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.Size = new System.Drawing.Size(200, 26);
+            this.modelTextBox.TabIndex = 12;
+            // 
+            // descTextBox
+            // 
+            this.descTextBox.Location = new System.Drawing.Point(173, 232);
+            this.descTextBox.Name = "descTextBox";
+            this.descTextBox.Size = new System.Drawing.Size(200, 26);
+            this.descTextBox.TabIndex = 13;
+            // 
+            // mdcTextBox
+            // 
+            this.mdcTextBox.Location = new System.Drawing.Point(477, 147);
+            this.mdcTextBox.Name = "mdcTextBox";
+            this.mdcTextBox.Size = new System.Drawing.Size(100, 26);
+            this.mdcTextBox.TabIndex = 11;
+            // 
+            // rwcCkBx
+            // 
+            this.rwcCkBx.AutoSize = true;
+            this.rwcCkBx.Location = new System.Drawing.Point(189, 301);
+            this.rwcCkBx.Name = "rwcCkBx";
+            this.rwcCkBx.Size = new System.Drawing.Size(63, 23);
+            this.rwcCkBx.TabIndex = 14;
+            this.rwcCkBx.Text = "RWC";
+            this.rwcCkBx.UseVisualStyleBackColor = true;
+            // 
+            // trwcCkBx
+            // 
+            this.trwcCkBx.AutoSize = true;
+            this.trwcCkBx.Location = new System.Drawing.Point(252, 301);
+            this.trwcCkBx.Name = "trwcCkBx";
+            this.trwcCkBx.Size = new System.Drawing.Size(72, 23);
+            this.trwcCkBx.TabIndex = 15;
+            this.trwcCkBx.Text = "TRWC";
+            this.trwcCkBx.UseVisualStyleBackColor = true;
+            // 
+            // mcwsCkBx
+            // 
+            this.mcwsCkBx.AutoSize = true;
+            this.mcwsCkBx.Location = new System.Drawing.Point(331, 301);
+            this.mcwsCkBx.Name = "mcwsCkBx";
+            this.mcwsCkBx.Size = new System.Drawing.Size(77, 23);
+            this.mcwsCkBx.TabIndex = 16;
+            this.mcwsCkBx.Text = "MCWS";
+            this.mcwsCkBx.UseVisualStyleBackColor = true;
+            // 
+            // naricsCkBx
+            // 
+            this.naricsCkBx.AutoSize = true;
+            this.naricsCkBx.Location = new System.Drawing.Point(415, 301);
+            this.naricsCkBx.Name = "naricsCkBx";
+            this.naricsCkBx.Size = new System.Drawing.Size(86, 23);
+            this.naricsCkBx.TabIndex = 17;
+            this.naricsCkBx.Text = "NARICS";
+            this.naricsCkBx.UseVisualStyleBackColor = true;
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(640, 70);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(80, 30);
+            this.newButton.TabIndex = 18;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(640, 110);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(80, 30);
+            this.addButton.TabIndex = 19;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(640, 150);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(80, 30);
+            this.searchButton.TabIndex = 20;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(640, 190);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(80, 30);
+            this.displayButton.TabIndex = 21;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(640, 270);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(80, 30);
+            this.exitButton.TabIndex = 23;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(640, 230);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(80, 30);
+            this.clearButton.TabIndex = 22;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // radioPictureBox
+            // 
+            this.radioPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("radioPictureBox.Image")));
+            this.radioPictureBox.Location = new System.Drawing.Point(477, 9);
+            this.radioPictureBox.Name = "radioPictureBox";
+            this.radioPictureBox.Size = new System.Drawing.Size(100, 125);
+            this.radioPictureBox.TabIndex = 24;
+            this.radioPictureBox.TabStop = false;
+            // 
+            // MilestonePrj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 361);
+            this.Controls.Add(this.radioPictureBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.naricsCkBx);
+            this.Controls.Add(this.mcwsCkBx);
+            this.Controls.Add(this.trwcCkBx);
+            this.Controls.Add(this.rwcCkBx);
+            this.Controls.Add(this.mdcTextBox);
+            this.Controls.Add(this.descTextBox);
+            this.Controls.Add(this.modelTextBox);
+            this.Controls.Add(this.convIDTextBox);
+            this.Controls.Add(this.trunkedIDTextBox);
+            this.Controls.Add(this.snTextBox);
+            this.Controls.Add(this.trunkSysLabel);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.modelNumLabel);
+            this.Controls.Add(this.mdcLabel);
+            this.Controls.Add(this.convIDLabel);
+            this.Controls.Add(this.trunkedIDLabel);
+            this.Controls.Add(this.snLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MilestonePrj";
+            this.Text = "Radio Inventory";
+            ((System.ComponentModel.ISupportInitialize)(this.radioPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label snLabel;
+        private System.Windows.Forms.Label trunkedIDLabel;
+        private System.Windows.Forms.Label convIDLabel;
+        private System.Windows.Forms.Label mdcLabel;
+        private System.Windows.Forms.Label modelNumLabel;
+        private System.Windows.Forms.Label descLabel;
+        private System.Windows.Forms.Label trunkSysLabel;
+        private System.Windows.Forms.TextBox snTextBox;
+        private System.Windows.Forms.TextBox trunkedIDTextBox;
+        private System.Windows.Forms.TextBox convIDTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.TextBox descTextBox;
+        private System.Windows.Forms.TextBox mdcTextBox;
+        private System.Windows.Forms.CheckBox rwcCkBx;
+        private System.Windows.Forms.CheckBox trwcCkBx;
+        private System.Windows.Forms.CheckBox mcwsCkBx;
+        private System.Windows.Forms.CheckBox naricsCkBx;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.PictureBox radioPictureBox;
+    }
+}
+
