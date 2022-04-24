03fd91225f832548dcc7728c71cb7289cbb84396
Brian Potter <90872695+CoyoteKatscher@users.noreply.github.com>
2022-04-24 16:23:47 -0700

    Add files via upload

---
+++ Milestone 5/Radio.cs
@@ -0,0 +1,43 @@
+﻿// Brian Potter
+// CST-150
+// 04/24/2022
+// The code is of my own work
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_5
+{
+    internal class Radio
+    {
+        public string serialNumber;
+        public string modelNumber;
+        public string description;
+        public string mdcID;
+        public int trunkedID;
+        public int conventionalID;
+        public bool systemRWC;
+        public bool systemTRWC;
+        public bool systemMCWS;
+        public bool systemNARICS;
+
+        public Radio ()
+        {
+            serialNumber = " ";
+            modelNumber = " ";
+            description = " ";
+            mdcID = " ";
+            trunkedID = 0;
+            conventionalID = 0;
+            systemRWC = false;
+            systemTRWC = false;
+            systemMCWS = false;
+            systemNARICS = false;
+        }
+
+    }
+}
---
+++ Milestone 5/readme.txt
@@ -0,0 +1,9 @@
+Brian Potter
+04/24/2022
+CST-150 Milestone 5
+
+Milestone 5 Rubric Requirements
+
+#1: Complete and present the Inventory Project
+Loom was used to create the video presentation.
+https://www.loom.com/share/e404081f213c47a0a6e60189e077e00b
